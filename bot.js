d8dfb7d121d900b4b469b1d804992032f3caf2d4
frasgamer <44925264+frasgamer@users.noreply.github.com>
2018-12-19 21:54:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "-";
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -12,3 +12,4 @@ client.on("ready", () => { // كود رينبو
   };
   setInterval(lol, 1000);
 });
+
